ea1a82ef2b4e01ce74a6f774dcbd0f1dae64aa3a
clasz1 <classzick@gmail.com>
2025-03-29 16:33:21 +0800

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'tbluser';
+    // column sa table 
+    protected $fillable = [
+        'username', 'password', 'gender'
+    ];
+
+    public $timestamps = false;
+    protected $primaryKey = 'userid';
+    protected $hidden = [
+        'password',
+    ];
+}
